13fda7fa10801f7b5407ea28cc9efdc7a9669def
AJKappes <alexander.j.kappes@gmail.com>
2020-07-20 13:29:33 -0700

    updated with posterior inference

--- r_scripts/ineff_loss_binlog_norm.R
+++ r_scripts/ineff_loss_binlog_norm.R
@@ -11,12 +11,23 @@ library(tidyverse)
 dt <- read_csv('data/ineff_loss_sym4.csv')
 dt$PoorCondition[which(dt$PoorCondition == 99)] <- 0
 
+# response label:
+## indicates whether household inefficiency is
+## greater than the mean (0) or less than the mean (1)
 y <- ifelse(dt$p_ineff >= mean(dt$p_ineff), 0, 1)
+
+edu <- ifelse(dt$prim_edu == 1 |
+                dt$sec_edu == 1, 1, 0)
 
 respskin <- ifelse(dt$RespDisorders == 1 |
                      dt$SkinDisorders == 1, 1, 0)
+
+# features:
+## household education level, 1 primary or sec edu, 0 otherwise
+## livestock general illness, 1 illness, 0 otherwise
+## respiratory/skin disorder, 1 if, 0 otherwise
 X <- matrix(cbind(1,
-                  dt$prim_edu,
+                  edu,
                   dt$GeneralIllness,
                   #dt$PoorCondition,
                   respskin),
@@ -100,12 +111,42 @@ while(err > tol & itr <= max_itr) {
   cat('Iteration', itr, 'max convergence error:', err,
       '\n')
   
+  if (err < tol) {
+    
+    beta_mp1var <- diag(beta_var(X, beta_mp1))
+    
+  }
+  
   beta_m <- beta_mp1
   itr <- itr + 1
   
 }
 
 
+
+# Posteriors --------------------------------------------------------------
+
+post_list <- vector(mode = 'list', length = length(beta_mp1))
+beta_post <- lapply(1:length(post_list),
+                    function(i)
+                      post_list[[i]] = rnorm(n = 1000,
+                                             mean = beta_mp1[i],
+                                             sd = sqrt(beta_mp1var[i])))
+# Parameter CIs
+tcrit <- qt(1-.05/2, dim(X)[1] - dim(X)[2])
+lb <- sapply(beta_post,
+             function(i) mean(i) - tcrit*sqrt(var(i)))
+ub <- sapply(beta_post,
+             function(i) mean(i) + tcrit*sqrt(var(i)))
+param_table <- data.frame(odds_coef = beta_mp1,
+                          lb = lb,
+                          ub = ub) %>% 
+  exp() %>% 
+  round(4)
+param_names <- c('int', 'edu', 'gen_illness', 'resp_skin_disorder')
+row.names(param_table) <- param_names
+
+ 
 
 # TODO --------------------------------------------------------------------
 
